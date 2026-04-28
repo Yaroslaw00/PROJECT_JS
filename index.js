ceae4d4a849f22b0f2d049f27453091197ec7ebe
Yaroslaw00 <yarikost901@gmail.com>
2026-04-28 20:35:32 +0200

    ok

--- index.js
+++ index.js
@@ -15,3 +15,34 @@ function calculator() {
 
 input1.addEventListener("input", calculator);
 input2.addEventListener("input", calculator);
+
+// (Камень, ножницы, бумага);
+
+const getStoneBtn = document.querySelector(".stone");
+const getScissorsBtn = document.querySelector(".scissors");
+const getPaperBtn = document.querySelector(".paper");
+const resultOfGame = document.querySelector(".result_game");
+const computerAnswer = document.querySelector(".computer_answer");
+
+const allAnswers = ["stone", "scissors", "paper"];
+
+function randomAnswer(answer) {
+  const computerChoice = Math.floor(Math.random() * 3);
+  const computerAnswer = allAnswers[computerChoice];
+  return computerAnswer;
+}
+
+getStoneBtn.addEventListener("click", (event) => {
+  const computerChoice = randomAnswer();
+  console.log(computerChoice);
+});
+
+getScissorsBtn.addEventListener("click", (event) => {
+  const computerChoice = randomAnswer();
+  console.log(computerChoice);
+});
+
+getPaperBtn.addEventListener("click", (event) => {
+  const computerChoice = randomAnswer();
+  console.log(computerChoice);
+});
