1bb5aebca52131e2a6ef9e4781a17040aea65d36
Yaroslaw00 <yarikost901@gmail.com>
2026-05-01 00:38:42 +0200

    ok

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const input1 = document.querySelector(".inp1");
 const input2 = document.querySelector(".inp2");
-const result = document.querySelector(".p");
-
-function calculator() {
-  const num1 = parseFloat(input1.value);
-  const num2 = parseFloat(input2.value);
-  if (!isNaN(num1) && !isNaN(num2)) {
-    const sum = num1 + num2;
-    result.textContent = `Результат: ${sum}`;
-  } else {
-    result.textContent = "Введите оба числа";
-  }
-}
-
 input1.addEventListener("input", calculator);
 input2.addEventListener("input", calculator);
+let result = document.querySelector(".p");
+const sume = document.querySelector(".plus");
+const subtract = document.querySelector(".minus");
+const multiply = document.querySelector(".multiplying");
+const divide = document.querySelector(".dividying");
+sume.addEventListener("click", calculator);
+subtract.addEventListener("click", calculator);
+multiply.addEventListener("click", calculator);
+divide.addEventListener("click", calculator);
+
+function calculator(event) {
+  const num1 = parseFloat(input1.value);
+  const num2 = parseFloat(input2.value);
+
+  const target = event.target;
+  if (isNaN(num1) || isNaN(num2)) {
+    result.textContent = "Введите корректное значение";
+    return;
+  }
+  if (target === sume) {
+    const fin = num1 + num2;
+    result.textContent = `Результат: ${fin}`;
+  } else if (target === subtract) {
+    const sub = num1 - num2;
+    result.textContent = `Результат: ${sub}`;
+  } else if (target === multiply) {
+    const mult = num1 * num2;
+    result.textContent = `Результат: ${mult}`;
+  } else if (target === divide && num2 !== 0) {
+    const divi = num1 / num2;
+    result.textContent = `Результат: ${divi}`;
+  }
+}
 
 // (Камень, ножницы, бумага);
 
